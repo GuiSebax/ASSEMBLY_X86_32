82ef3963aacd8fbbc4838c6c64fc74a8558dda6b
Guilherme-Clemente <103221587+GuiSebax@users.noreply.github.com>
2024-07-26 23:06:36 -0300

    Update trabalho.s

--- trabalho.s
+++ trabalho.s
@@ -11,20 +11,21 @@ nova_linhas: .string "\n"
 menu: .string "Menu:\n1 - Preencher matriz NxM\n2 - Buscar elemento na matriz\n3 - Mostrar diagonal principal\n4 - Mostrar lucky numbers\n5 - Sair\nEscolha uma opção: "
 opcao_invalidas: .string "Opção inválida!\n"
 msg_sair: .string "Saindo... Tenha um bom dia!\n"
-buscar_linha: .string "Digite a linha do elemento a buscar: "
-buscar_coluna: .string "Digite a coluna do elemento a buscar: "
-elemento_encontrado: .string "Elemento encontrado: %d\n"
+buscar_elemento_msg: .string "Digite o elemento que queira buscar: "
+elemento_encontrado: .string "Elemento %d encontrado na posição [%d][%d]\n"
+elemento_n_encontrado: .string "Elemento %d não foi encontrado na matriz\n"
 lucky_number_msg: .string "Lucky number encontrado: %d\n"
 mostrar_matriz_msg: .string "Matriz (%d x %d):\n"
-debug_msg: .string "Valor lido: %d\n"
-debug_store: .string "Armazenando na matriz[%d][%d]: %d\n"
-debug_matrix: .string "Valor na matriz[%d][%d]: %d\n"
-
-.section .bss
+menor_linhas_msg: .string "Menor elemento da linha %d é %d\n"
+maior_colunas_msg: .string "Maior elemento da coluna %d é %d\n"
 matriz: .int 0  # Posição para armazenar o endereço da matriz
+menor_valores: .space 4000  # Vetor para armazenar os menores valores das linhas
+maior_valores: .space 4000  # Vetor para armazenar os maiores valores das colunas
 opcao: .int 0
+elemento_busca: .int 0
 linha_busca: .int 0
 coluna_busca: .int 0
+
 
 .section .text
 .globl _start
@@ -119,13 +120,6 @@ preencher_colunas:
     call scanf
     addl $8, %esp
 
-    # Depuração: Imprimir o valor lido
-    movl val_temp, %eax
-    pushl %eax
-    pushl $debug_msg
-    call printf
-    addl $8, %esp
-
     # Armazenar na matriz
     movl val_temp, %edx  # Carrega o valor lido de val_temp
     movl %esi, %eax      # i
@@ -134,14 +128,6 @@ preencher_colunas:
     imull $4, %eax       # Multiplicar por 4 para obter o deslocamento em bytes
     movl matriz, %ebx    # Carregar o endereço da matriz
     movl %edx, (%ebx, %eax)  # Armazena valor
-
-    # Depuração: Imprimir a operação de armazenamento
-    pushl %edx
-    pushl %edi
-    pushl %esi
-    pushl $debug_store
-    call printf
-    addl $16, %esp
 
     incl %edi
     jmp preencher_colunas
@@ -175,14 +161,6 @@ imprimir_colunas:
     movl matriz, %ebx    # Carregar o endereço da matriz
     movl (%ebx, %eax), %edx  # Carregar matriz[i][j]
 
-    # Depuração: Imprimir o valor lido da matriz
-    pushl %edx
-    pushl %edi
-    pushl %esi
-    pushl $debug_matrix
-    call printf
-    addl $16, %esp
-
     pushl %edx           # Valor a ser impresso
     pushl $input_valor   # Formato
     call printf
@@ -203,43 +181,61 @@ voltar_menu:
     call mostrar_menu
 
 buscar_elemento:
-    pushl $buscar_linha
+    pushl $buscar_elemento_msg
     call printf
     addl $4, %esp
 
-    movl $linha_busca, %eax
+    movl $elemento_busca, %eax
     pushl %eax
     pushl $input_valor
     call scanf
     addl $8, %esp
 
-    pushl $buscar_coluna
-    call printf
-    addl $4, %esp
+    # Inicializar índices de busca
+    movl $0, %esi  # i = 0
+buscar_linhas:
+    cmpl linhas, %esi
+    jge elemento_n_encontrado  # Se i >= linhas, elemento não encontrado
 
-    movl $coluna_busca, %eax
-    pushl %eax
-    pushl $input_valor
-    call scanf
-    addl $8, %esp
-
-    # Buscar o elemento
-    movl linha_busca, %esi
-    movl coluna_busca, %edi
+    movl $0, %edi  # j = 0
+buscar_colunas:
+    cmpl colunas, %edi
+    jge incrementar_linha_busca  # Se j >= colunas, vai para próxima linha
 
     movl %esi, %eax      # i
-    imull colunas        # i * colunas
+    imull colunas, %eax  # i * colunas
     addl %edi, %eax      # i * colunas + j
-    imull $4, %eax       # Multiplicar por 4 para obter o deslocamento em bytes
+    shl $2, %eax         # Multiplicar por 4 para obter o deslocamento em bytes
     movl matriz, %ebx    # Carregar o endereço da matriz
-    movl (%ebx, %eax), %edx  # Carregar matriz[i][j]
+    movl (%ebx, %eax), %ecx  # Carregar matriz[i][j]
 
-    pushl %edx           # Valor a ser impresso
-    pushl $elemento_encontrado # Mensagem
+    cmpl elemento_busca, %ecx
+    jne continuar_busca
+
+    # Elemento encontrado
+    pushl %edi  # j
+    pushl %esi  # i
+    pushl elemento_busca  # elemento
+    pushl $elemento_encontrado
+    call printf
+    addl $16, %esp
+    jmp voltar_menu
+
+continuar_busca:
+    incl %edi
+    jmp buscar_colunas
+
+incrementar_linha_busca:
+    incl %esi
+    jmp buscar_linhas
+
+elemento_nao_encontrado:
+    pushl elemento_busca  # elemento
+    pushl $elemento_n_encontrado
     call printf
     addl $8, %esp
 
-    call mostrar_menu
+    call voltar_menu
 
 mostrar_diagonal:
     movl $0, %esi  # i = 0
@@ -269,64 +265,147 @@ imprimir_diagonal:
     jmp imprimir_diagonal
 
 mostrar_lucky_numbers:
-    movl $0, %esi  # i = 0
-verificar_linhas:
-    cmpl linhas, %esi
-    jge voltar_menu  # Se i >= linhas, sai do loop
+    # Inicializar os vetores de menores e maiores valores
+    movl linhas, %eax
+    shl $2, %eax          # Multiplicar por 4 (tamanho de um inteiro)
+    movl $0, %esi         # i = 0
+    lea menor_valores, %edi
 
-    # Encontrar o menor elemento na linha i
-    movl $0, %edi
-    movl $2147483647, %ebx  # Inicializa com o maior valor de int
+    # Inicializar menor_valores com o maior valor possível
+inicializar_menores:
+    movl $2147483647, (%edi, %esi, 4)
+    incl %esi
+    cmpl linhas, %esi
+    jl inicializar_menores
+
+    movl colunas, %eax
+    shl $2, %eax          # Multiplicar por 4 (tamanho de um inteiro)
+    movl $0, %esi         # i = 0
+    lea maior_valores, %edi
+
+    # Inicializar maior_valores com o menor valor possível
+inicializar_maiores:
+    movl $-2147483648, (%edi, %esi, 4)
+    incl %esi
+    cmpl colunas, %esi
+    jl inicializar_maiores
+
+    # Encontrar o menor valor de cada linha
+    movl $0, %esi  # i = 0
+    movl linhas, %ebx
+    movl matriz, %ecx
+encontrar_menores:
+    cmpl %ebx, %esi
+    jge verificar_maiores  # Se i >= linhas, vai verificar os maiores
+
+    movl $0, %edi  # j = 0
+    lea menor_valores, %edx
 
 encontrar_menor_na_linha:
     cmpl colunas, %edi
-    jge verificar_linha_atual
+    jge armazenar_menor  # Se j >= colunas, armazena o menor valor da linha
 
     movl %esi, %eax      # i
-    imull colunas        # i * colunas
+    imull colunas, %eax  # i * colunas
     addl %edi, %eax      # i * colunas + j
-    imull $4, %eax       # Multiplicar por 4 para obter o deslocamento em bytes
+    shl $2, %eax         # Multiplicar por 4 para obter o deslocamento em bytes
     movl matriz, %ebx    # Carregar o endereço da matriz
-    movl (%ebx, %eax), %edx  # Carregar matriz[i][j]
+    movl (%ebx, %eax), %eax  # Carregar matriz[i][j]
 
-    cmpl %edx, %ebx
-    jge continuar_linha
-    movl %edx, %ebx      # Atualiza o menor elemento
+    cmpl %eax, (%edx, %esi, 4)
+    jge continuar_menor_na_linha
+    movl %eax, (%edx, %esi, 4)  # Atualiza o menor elemento
 
-continuar_linha:
+continuar_menor_na_linha:
     incl %edi
     jmp encontrar_menor_na_linha
 
-verificar_linha_atual:
-    # Verificar se o menor elemento na linha é o maior na coluna
-    movl $0, %edi
-verificar_coluna:
+armazenar_menor:
+    incl %esi
+    jmp encontrar_menores
+
+verificar_maiores:
+    # Encontrar o maior valor de cada coluna
+    movl $0, %esi  # j = 0
+    movl colunas, %ebx
+    movl matriz, %ecx
+encontrar_maiores:
+    cmpl %ebx, %esi
+    jge verificar_lucky_numbers  # Se j >= colunas, vai verificar os lucky numbers
+
+    movl $0, %edi  # i = 0
+    lea maior_valores, %edx
+
+encontrar_maior_na_coluna:
     cmpl linhas, %edi
-    jge proxima_linha
+    jge armazenar_maior  # Se i >= linhas, armazena o maior valor da coluna
 
-    movl %edi, %eax      # j
-    imull colunas        # j * colunas
-    addl %esi, %eax      # j * colunas + i
-    imull $4, %eax       # Multiplicar por 4 para obter o deslocamento em bytes
+    movl %edi, %eax      # i
+    imull colunas, %eax  # i * colunas
+    addl %esi, %eax      # i * colunas + j
+    shl $2, %eax         # Multiplicar por 4 para obter o deslocamento em bytes
     movl matriz, %ebx    # Carregar o endereço da matriz
-    movl (%ebx, %eax), %edx  # Carregar matriz[j][i]
+    movl (%ebx, %eax), %eax  # Carregar matriz[i][j]
 
-    cmpl %ebx, %edx
-    jge continuar_coluna
-    jmp proxima_linha
+    cmpl (%edx, %esi, 4), %eax
+    jle continuar_maior_na_coluna
+    movl %eax, (%edx, %esi, 4)  # Atualiza o maior elemento
 
-continuar_coluna:
+continuar_maior_na_coluna:
     incl %edi
-    jmp verificar_coluna
+    jmp encontrar_maior_na_coluna
 
-proxima_linha:
-    pushl %ebx           # Lucky number a ser impresso
-    pushl $lucky_number_msg # Mensagem
+armazenar_maior:
+    incl %esi
+    jmp encontrar_maiores
+
+verificar_lucky_numbers:
+    # Verificar lucky numbers
+    pushl $lucky_number_msg
+    call printf
+    addl $4, %esp
+
+    movl $0, %esi  # i = 0
+    movl linhas, %eax
+    lea menor_valores, %ecx
+    lea maior_valores, %edx
+verificar_lucky_numbers_loop:
+    cmpl %eax, %esi
+    jge fim_lucky_numbers  # Se i >= linhas, sai do loop
+
+    movl (%ecx, %esi, 4), %ebx  # Carregar menor_valores[i]
+    movl $0, %edi  # j = 0
+
+comparar_maior:
+    cmpl colunas, %edi
+    jge incrementar_linha_lucky  # Se j >= colunas, vai para próxima linha
+
+    movl (%edx, %edi, 4), %eax  # Carregar maior_valores[j]
+    cmpl %ebx, %eax
+    jne continuar_comparar_maior
+
+    # Lucky number encontrado
+    pushl %ebx
+    pushl $input_valor
     call printf
     addl $8, %esp
 
+continuar_comparar_maior:
+    incl %edi
+    jmp comparar_maior
+
+incrementar_linha_lucky:
     incl %esi
-    jmp verificar_linhas
+    jmp verificar_lucky_numbers_loop
+
+fim_lucky_numbers:
+    pushl $nova_linhas
+    call printf
+    addl $4, %esp
+
+    jmp voltar_menu
+
+
 
 sair:
     pushl $msg_sair
